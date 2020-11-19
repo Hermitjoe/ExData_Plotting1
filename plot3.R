528d1f8a7315d5fbe17d09ac94411cbdb2888f43
Joseph Matteson <74080675+Hermitjoe@users.noreply.github.com>
2020-11-19 17:59:46 -0500

    Update
    Forgot Legend

--- plot3.R
+++ plot3.R
@@ -22,4 +22,12 @@ with(ed, {
   lines(DateTime, Sub_metering_3, col = "Blue")
 })
 
+
+legend("topright", col=c("Red", "Green", "Blue"), lty=1, lwd=2, bty="n",
+       legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
 ##Note: I am colorblind, so the color may be off 
+
+## Put the plot into a file
+dev.copy(png, file= 'plot3.png')                     # Copy displayed plot to a file
+dev.off()                                            # Close File to make it available
